0afecd5755039b32bdb53ab39027220b1c2f6442
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2023-06-18 08:19:05 -0300

    se generara precistencia de datos por medio de LOCALSTORAGE

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -1,19 +1,30 @@
 import { Header } from "./components/Header";
 import { ListadoPaciente } from "./components/ListadoPaciente";
 import { Formulario } from "./components/Formulario";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
   const [pacientes,setPacientes] = useState([])
   const [unPaciente, setUnPaciente]= useState({})
 
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? []; // si no hay nada crea un array vacio
+      setPacientes(pacientesLS)
+    }
+    obtenerLS();
+  }, []);
+  
+  useEffect(()=>{
+    localStorage.setItem('pacientes',JSON.stringify(pacientes))
+  },[pacientes])
+  
   const eliminarPaciente=(id)=>{
     const actualisarPaciente = pacientes.filter(pte=> pte.id !== id)
     setPacientes(actualisarPaciente)
 
   }
-
   return (
     <div className="container mx-auto mt-20">
       <Header></Header>
